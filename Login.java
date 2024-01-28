9f23f26079fec83c55d5b456bd5f1376d1141f27
mohdnadeemece <140728257+mohdnadeemece@users.noreply.github.com>
2024-01-29 03:27:11 +0530

    This approach which i am using.

--- Login.java
+++ Login.java
@@ -1,13 +1,14 @@
-package com.egapy.create.account;
+package com.egapy.login;
+
+import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
@@ -15,42 +16,41 @@ import org.testng.annotations.Test;
 public class Login {
 	
 	WebDriver driver;
-	
 	WebDriverWait wait;
+	
 	
 	@Parameters("browser")
 	@BeforeClass
-	
-	public void log (@Optional ("chrome") String browser) {
+
+	public void loading (@Optional("chrome") String browser) {
 		
-		if (browser.equalsIgnoreCase("chrome")){
-			
-			driver =new ChromeDriver();
-			}
-		if (browser.equalsIgnoreCase("firefox")) {
-			driver = new FirefoxDriver();
-		}
-		if (browser.equalsIgnoreCase("edge")) {
-			driver = new EdgeDriver();
+		if (browser.equalsIgnoreCase("chrome")) {
+			driver= new ChromeDriver();
 		}
 		
-		driver.get("https://www.egapy.com/");
-		driver.manage().window().maximize();
-		
+//		driver.get("https://www.egapy.com/");
+//		driver.manage().window().maximize();
+//		driver.findElement(By.xpath("//*[@id=\"shopify-section-sections--18981293490464__"
+//				+ "ccfafd7a-7166-43fa-9673-9be28b294582\"]/sticky-header/div/div/div[3]/a[1]")).click();
 		
 	}
 	
-	@Test
-	public void logincred () {
+	
+	@Test(dataProvider = "getData")
+	public void loginpage (String Emailaddress, String pass) {
 		
-		driver.findElement(By.xpath("//*[@id=\"shopify-section-sections--18981293490464__ccfafd7a-7166-43fa-9673-9be28b294582\"]/sticky-header/div/div/div[3]/a[1]")).click();
+		driver.get("https://www.egapy.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//*[@id=\"shopify-section-sections--18981293490464__"
+				+ "ccfafd7a-7166-43fa-9673-9be28b294582\"]/sticky-header/div/div/div[3]/a[1]")).click();
+
 		
 		WebElement emailadd = driver.findElement(By.xpath("//*[@id=\"customer_email\"]"));
 		
-		emailadd.sendKeys("relxationnature@gmail.com");
+		emailadd.sendKeys(Emailaddress);
 		
 		WebElement paswd = driver.findElement(By.xpath("//*[@id=\"customer_password\"]"));
-		paswd.sendKeys("ashu0000");
+		paswd.sendKeys( pass);
 		
 		driver.findElement(By.xpath("//*[@value=\"Log in\"]")).click();
 		
@@ -58,8 +58,50 @@ public class Login {
 		
 	}
 	
-	
-	
+	@DataProvider
+	@Test
+	Object[][] getData()
+
+	{
+		 
+		Object[][] data = new Object[4][2]; 
+		
+		// valid user-name valid password
+		data[0][0] = "relxationnature@gmail.com";
+		data[0][1] = "ashu0000"; 
+		
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		// valid user-name and invalid password 
+		data[1][0] = "relxationnature@gmail.com";
+		data[1][1] = "hgsuyhgf"; 
+		
+		
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		// invalid user-name and valid password 
+		data[2][0] = "shanmohd954@gmail.com";
+		data[2][1] = "ashu0000"; 
+		
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		// invalid user-name and valid password 
+		data[3][0] = "shanmohd954@gmail.com";
+		data[3][1] = "jhdfuyfguy"; 
+		
+		return data;
+		 
+	} 
 	
 
 }
+
+
+  
+  
+
+
+
+
+
+
+
+
+
